57ded92b786b62a07be96108205a814a5c84c540
Rafal Wolak <wolak.rafal@gmail.com>
2015-10-08 20:11:40 +0200

    recursion session - end.

--- src/hhfuns/hhfuns.erl
+++ src/hhfuns/hhfuns.erl
@@ -39,3 +39,72 @@ filter(Pred, L) ->
                 end
   end,
   reverse(fold(F,[],L)).
+
+one() -> 1.
+two() -> 2.
+
+add(X, Y) -> X() + Y().
+
+map(_, []) -> [];
+map(F, [H | T]) -> [F(H) | map(F, T)].
+
+incr(X) -> X + 1.
+decr(X) -> X - 1.
+
+%%%% Anonymous functions
+%% fun(Args1) ->
+%%       Expression1, Exp2, ..., ExpN;
+%%     (Args2) ->
+%%       Expression1, Exp2, ..., ExpN;
+%%     (Args3) ->
+%%       Expression1, Exp2, ..., ExpN
+%% end
+%% Fn = fun() -> a end.
+
+%%%% It is important to note that the inherited scope follows the anonymous function wherever it is,
+%%%% even when it is passed to another function:
+
+a() ->
+  Secret = "pony",
+  fun() -> Secret end.
+
+b(F) ->
+  "a/0's password is "++F().
+
+%%%% TRAP:
+%A little trap you might fall into when writing
+%% anonymous functions is when you try to redefine the scope:
+
+
+base_wrong() ->
+  A = 1,
+  (fun() -> A = 2 end)(). % Will fail
+
+base_shadowing() ->
+  A = 1,
+  (fun(A) -> A = 2 end)(2).
+
+%% only keep even numbers
+even(L) -> lists:reverse(even(L, [])).
+even([], Acc) -> Acc;
+even([H | T], Acc) when H rem 2 == 0 -> even(T, [H | Acc]);
+even([_ | T], Acc) -> even(T, Acc).
+
+%% only keep men older than 60
+old_men(L) -> lists:reverse(old_men(L,[])).
+
+old_men([], Acc) -> Acc;
+old_men([Person = {male, Age}|People], Acc) when Age > 60 -> old_men(People,[Person|Acc]);
+old_men([_ |People], Acc) -> old_men(People,Acc).
+
+filter2(Pred, L) -> lists:reverse(filter(Pred, L, [])).
+
+filter(_, [], Acc) -> Acc;
+%% filter(Pred, [H | T], Acc) when Pred(H) -> filter(Pred, T, [H | Acc]);
+%% filter(Pred, [_ | T], Acc) -> filter(Pred, T, Acc).
+% using case
+filter(Pred, [H | T], Acc) ->
+  case Pred(H) of
+    true -> filter(Pred, T, [H | Acc]);
+    false -> filter(Pred, T, Acc)
+  end.
