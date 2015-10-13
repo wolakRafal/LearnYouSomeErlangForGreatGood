036b7dfbd9d534c19136b708a8faf5e0cbd7bca5
Rafal Wolak <RWolak@advaoptical.com>
2015-10-13 23:28:54 +0200

    process - links

---
+++ src/processes/linkmon.erl
@@ -0,0 +1,30 @@
+%%%-------------------------------------------------------------------
+%%% @author Rafal Wolak
+%%% @copyright (C) 2015, robo software innovations
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. Oct 2015 23:13
+%%%-------------------------------------------------------------------
+-module(linkmon).
+-author("Rafal Wolak").
+
+%% API
+-compile(export_all).
+
+myproc() ->
+  timer:sleep(5000),
+  exit(reason).
+
+chain(0) ->
+  receive
+    _ -> ok
+  after 2000 ->
+    exit("chain dies here")
+  end;
+chain(N) ->
+  Pid = spawn(fun() -> chain(N-1) end),
+  link(Pid),
+  receive
+    _ -> ok
+  end.
